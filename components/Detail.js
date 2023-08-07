1a1bfe8393b4fa4213f13cabe2a55ba4a11bfb9b
kayan9896 <a4ssgit@hotmail.com>
2023-08-07 13:46:18 -0700

    add commnt box

--- components/Detail.js
+++ components/Detail.js
@@ -1,14 +1,26 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, FlatList } from 'react-native'
+import React, { useEffect } from 'react'
 import Input from './Input'
-
+import { collection,onSnapshot,query, where } from 'firebase/firestore'
+import { db } from '../firebase/setup.js'
 
 export default function Detail({route}) {
+  const [cms, setCms] = React.useState([])
+  useEffect(function(){
+    const dt=onSnapshot(query(collection(db,"comments"),where("mv",'==',route.params.info.name)),function(q){
+      if(!q.empty){
+        const puredt=q.docs.map(function(doc){return doc.data();})
+        setCms(puredt);
+      }else{setCms([]);}
+    })
+    return function(){dt()}
+},[])
   return (
-    <View>
+    <View style={{alignItems:'center'}}>
       <Text>{route.params.info.name}</Text>
       
-      <Input/>
+      <Input mvname={route.params.info.name}/>
+      <FlatList data={cms} renderItem={(i)=>{return <Text>{i.item.cm}</Text>}}/>
     </View>
   )
 }
--- components/Input.js
+++ components/Input.js
@@ -2,14 +2,14 @@ import { View, Text, TextInput, Pressable } from 'react-native'
 import React from 'react'
 import {add,remove} from '../firebase/util.js'
 
-export default function Input() {
+export default function Input({mvname}) {
     const [text, setText] = React.useState('')
   return (
     <View>
         <Text>Leave your comment</Text>
       <TextInput placeholder="Comment" onChangeText={function(tx){setText(tx)}}/>
       <Pressable onPress={function(){
-        add({cm:text});
+        add("comments",{cm:text,mv:mvname});
       }}><Text>Post</Text></Pressable>
     </View>
   )
