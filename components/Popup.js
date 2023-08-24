becef0d6d8f92837afd48bbc3503b81d0c682800
ziqifo <fo.z@northeastern.edu>
2023-08-23 19:35:41 -0700

    Improve the style of popup

--- components/Popup.js
+++ components/Popup.js
@@ -2,57 +2,103 @@ import { View, Text, Modal, Pressable, StyleSheet } from 'react-native';
 import React from 'react';
 import Signup from './Signup';
 import Login from './Login';
+import { LinearGradient } from 'expo-linear-gradient';
+import Color from './Color';
 
 export default function Popup({ vis, changevis }) {
   const [isSignup, setIsSignup] = React.useState(true);
+  
   return (
-    <Modal visible={vis} >
-      <View style={styles.container}>
-        <Pressable onPress={() => { changevis(false); }} style={styles.closeButton}>
-          <Text>X close</Text>
-        </Pressable>
-        <View style={styles.buttonContainer}>
-          <Pressable onPress={function () { setIsSignup(true); }} style={[styles.button, isSignup && styles.activeButton]}>
-            <Text style={isSignup ? styles.activeText : styles.inactiveText}>Signup</Text>
+    <Modal visible={vis} transparent={true} animationType="slide">
+      <View style={styles.overlay}>
+        <LinearGradient 
+          colors={[Color.gradientStart, Color.gradientEnd, Color.gradientFinal]}
+          style={styles.modalContainer}>
+          <Pressable onPress={() => { changevis(false); }} style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>Don't want to be a member?</Text>
           </Pressable>
-          <Pressable onPress={function () { setIsSignup(false); }} style={[styles.button, !isSignup && styles.activeButton]}>
-            <Text style={!isSignup ? styles.activeText : styles.inactiveText}>Login</Text>
-          </Pressable>
-        </View>
-        {isSignup ? <Signup fail={changevis} /> : <Login fail={changevis} />}
+          <View style={styles.buttonContainer}>
+            <Pressable onPress={() => setIsSignup(true)} style={[styles.button, isSignup && styles.activeButton]}>
+              <Text style={isSignup ? styles.activeText : styles.inactiveText}>Signup</Text>
+            </Pressable>
+            <Pressable onPress={() => setIsSignup(false)} style={[styles.button, !isSignup && styles.activeButton]}>
+              <Text style={!isSignup ? styles.activeText : styles.inactiveText}>Login</Text>
+            </Pressable>
+          </View>
+          {isSignup ? <Signup fail={changevis} /> : <Login fail={changevis} />}
+        </LinearGradient>
       </View>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    justifyContent: 'center',
+  },
+  modalContainer: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
   container: {
     marginTop: 40,
     alignItems: 'center',
     flex: 1,
     padding: 20,
+    backgroundColor: Color.gradientFinal,
   },
   closeButton: {
     alignSelf: 'flex-end',
     margin: 10,
+  },
+  closeButtonText: {
+    fontSize: 15,
+    color: '#555',
+    textDecorationLine: 'underline',
   },
   buttonContainer: {
     flexDirection: 'row',
     borderColor: 'grey',
     borderBottomWidth: 2,
     marginBottom: 20,
+    width: '100%',
+    justifyContent: 'center',
   },
   button: {
-    margin: 20,
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Color.Button,
+    borderRadius: 5,
+    marginHorizontal: 5,
   },
   activeButton: {
     borderBottomWidth: 3,
-    borderBottomColor: 'dodgerblue',
+    borderBottomColor: Color.Header,
+    elevation: 5, 
+    shadowColor: '#000', 
+    shadowOffset: { width: 0, height: 2 }, 
+    shadowOpacity: 0.5, 
+    shadowRadius: 10,
   },
   activeText: {
-    color: 'dodgerblue',
+    color: 'white',
+    fontSize: 18,
   },
   inactiveText: {
-    color: 'grey',
+    color: 'white',
+    fontSize: 18,
   },
 });
